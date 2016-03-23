8208696456c97469b4d1c9f743b6f01bb96242bd
kickinbahk <kickinbahk@gmail.com>
2016-03-23 07:17:50 -0700

    add socket.io

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var PORT = process.env.PORT || 3000
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
+var io = require('socket.io')
 
 app.use(express.static(`${__dirname}/public`))
 http.listen(PORT, function () {
