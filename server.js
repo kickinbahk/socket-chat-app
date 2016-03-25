b2208f8fd9852f01e2d6049a5ef569df32f7b17d
kickinbahk <kickinbahk@gmail.com>
2016-03-25 09:46:13 -0700

    set up basic communictation from server to client

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ var io = require('socket.io')(http)
 
 app.use(express.static(`${__dirname}/public`))
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
   console.log('User connected via socket.io')
+  socket.emit('message', {
+    text: 'Welcome to the chat app'
+  })
 })
 
 http.listen(PORT, function () {
