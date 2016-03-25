17fe3d2f6089b5d51cc1714e8aa9da32730ca640
kickinbahk <kickinbahk@gmail.com>
2016-03-25 12:04:40 -0700

    add timestamp to displayed message

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,11 @@ socket.on('connect', function () {
 })
 
 socket.on('message', function (message) {
+  var momentTimestamp = moment.utc(message.timestamp)
   console.log('New Message:')
   console.log(message.text)
 
-  $('.messages').append(`<p>${message.text}</p>`)
+  $('.messages').append(`<p><strong>${momentTimestamp.local().format('h:mm:a')}:</strong> ${message.text}</p>`)
 })
 
 // New message submission
